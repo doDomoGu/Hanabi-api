d453e189ec2031ea9e2afa70236afdb74640f9fb
doDomoGu <kaka_71936410@qq.com>
2018-02-12 23:14:48 +0800

    游戏挂机 命令行 计划任务

---
+++ commands/GameController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+class GameController extends Controller
+{
+    public function actionCheck()
+    {
+        echo "12313123 \n";
+    }
+}
