ac3428aba218964d090c2089d2982b9951ca3011
doDomoGu <kaka_71936410@qq.com>
2018-02-17 23:42:21 +0800

    curl post请求 封装

--- commands/GameController.php
+++ commands/GameController.php
@@ -4,6 +4,7 @@ namespace app\commands;
 
 use Yii;
 use yii\console\Controller;
+use app\components\CurlRequest;
 
 class GameController extends Controller
 {
@@ -17,39 +18,11 @@ class GameController extends Controller
 //        $post_data['password']    = '123456';
 //        $post_data['email']    = 'zsjs124@126.com';
         //$post_data = array();
-        $res = $this->request_post($url, $post_data);
+        $res = CurlRequest::postRequest($url, $post_data);
         print_r($res);
+        echo "\n";
 
     }
 
-    /**
-    * 模拟post进行url请求
-    * @param string $url
-    * @param array $post_data
-    */
-    private function request_post($url = '', $post_data = array()) {
-        if (empty($url) ) {
-            return false;
-        }
 
-        $o = "";
-        foreach ( $post_data as $k => $v )
-        {
-            $o.= "$k=" . urlencode( $v ). "&" ;
-        }
-        $post_data = substr($o,0,-1);
-
-        $postUrl = $url;
-        $curlPost = $post_data;
-        $ch = curl_init();//初始化curl
-        curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
-        $data = curl_exec($ch);//运行curl
-        curl_close($ch);
-
-        return $data;
-    }
 }
